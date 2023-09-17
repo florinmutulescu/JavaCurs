20d34ddd85123086aea498fb63aa0539b12d5efa
MUTULESCU FLORIN <florinmutulescu@yahoo.com>
2023-09-17 19:29:14 +0300

    curs4 tema corectie

--- src/curs4/PunctajTema.java
+++ src/curs4/PunctajTema.java
@@ -12,21 +12,23 @@ public class PunctajTema {
 		System.out.println("Va rog intrduceti punctajul:");
 		valPunctaj = scan.nextInt();
 		scan.close();
+		String result = (valPunctaj >= 90) ? "FB" : (valPunctaj >= 80) ? "B" : "S";
+		System.out.println("Ai primit calificativul:"
+				+ (result.equals("FB") ? "Foarte bine" : (result.equals("B") ? "Bine" : "Suficient")));
 
-		if (valPunctaj >= 90) {
-			punctaj = "FB";
+		// if (valPunctaj >= 90) {
+		// punctaj = "FB";
 
-			System.out.println("Punctajul este: " + punctaj + "   Ai primit: Foarte Bine!");
-		} else if (valPunctaj >= 80) {
-			punctaj = "B";
+		// System.out.println("Punctajul este: " + punctaj + " Ai primit: Foarte
+		// Bine!");
+		// } else if (valPunctaj >= 80) {
+		// punctaj = "B";
 
-			System.out.println("Punctajul este: " + punctaj + "  Ai primit: Bine!");
-		} else {
-			punctaj = "S";
+		// System.out.println("Punctajul este: " + punctaj + " Ai primit: Bine!");
+		// } else {
+		// punctaj = "S";
 
-			System.out.println("Punctajul este: " + punctaj + "  Ai primit : suficient!");
-
-		}
+		// System.out.println("Punctajul este: " + punctaj + " Ai primit : suficient!");
 
 	}
 
--- src/curs4/TernaryOperatorTema.java
+++ src/curs4/TernaryOperatorTema.java
@@ -8,14 +8,14 @@ public class TernaryOperatorTema {
 
 	public static void main(String[] args) {
 
-		int comision;
+		int comision = 0;
 		int vanzari = 3500;
 		// comision = (vanzari > 2500) ? vanzari * 5 / 100 : 0;
 		if (vanzari > 2500) {
 			comision = vanzari * 5 / 100;
-		} else {
-			comision = 0;
-		}
+		} // else {
+			// comision = 0;
+			// }
 
 		System.out.println("Comisionul tau este :" + comision);
 
