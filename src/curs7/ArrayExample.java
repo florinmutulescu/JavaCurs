33531ee9141200a26857863033505f1ea71e95aa
MUTULESCU FLORIN <florinmutulescu@yahoo.com>
2023-09-25 21:33:55 +0300

    curs7 final

--- src/curs7/ArrayExample.java
+++ src/curs7/ArrayExample.java
@@ -3,8 +3,17 @@ package curs7;
 public class ArrayExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[] textArray = new String[4];
+		// String[] textArray ={null,null,null,null}
+		// index: 0 1 2 3
+		System.out.println(textArray[0]);
+		textArray[0] = "this";
+		System.out.println("n----------------");
+		// for each
+		for (String element : textArray) {
+			System.out.println(element);
 
+		}
 	}
 
 }
